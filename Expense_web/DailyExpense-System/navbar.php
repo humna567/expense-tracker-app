b2e2fbf418bf1923483efde0ffd95e95104ed2a9
Humna Ghaffar <88514009+humna567@users.noreply.github.com>
2023-08-08 01:04:28 +0500

    Add files via upload

---
+++ Expense_web/DailyExpense-System/navbar.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>EXPENSE TRACKER WEB</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top  header-transparent ">
+        <div class="container d-flex align-items-center justify-content-between">
+
+            <div class="logo">
+                <h1><a href="navbar.html">BudgetBuddy</a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+            </div>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#features">App Features</a></li>
+                    <li><a class="nav-link scrollto" href="#gallery">About</a></li>
+                    <li><a class="nav-link scrollto" href="#Services">Services</a></li>
+                    <li><a class="getstarted scrollto" href="#features" style='color:white;'>Get Started</a></li>
+                    
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Dashboard Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1"
+                    data-aos="fade-up">
+                    <div>
+                        <h1>WELCOME DASHBOARD</h1>
+                        <h5>Your financial health, our priority: Track expenses like a pro </h5>
+                        <BR></BR>
+                        <a href="DailyExpense-System/login.php" class="download-btn"><i class="bi bi-lock"></i> Login</a>
+                        <!-- <a href="login.php" class="download-btn"><i class="custom-login-img"></i> Login</a> -->
+                        <a href="DailyExpense-System/register.php" class="download-btn"><i class="bi bi-key"></i> Register</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img"
+                    data-aos="fade-up">
+                    <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+        <!-- ======= App Features Section ======= -->
+        <section id="features" class="features">
+            <div class="container">
+
+                <div class="section-title">
+                    <h2>App Features</h2>
+                    <p>An expense tracker app typically offers several features to help users effectively manage their
+                        finances and track their
+                        expenses. Here are some common features found in expense tracker apps</p>
+                </div>
+
+                <div class="row no-gutters">
+                    <div class="col-xl-7 d-flex align-items-stretch order-2 order-lg-1">
+                        <div class="content d-flex flex-column justify-content-center">
+                            <div class="row">
+                                <div class="col-md-6 icon-box" data-aos="fade-up">
+                                    <i class="bx bx-receipt"></i>
+                                    <h4>Expense Logging</h4>
+                                    <p>The app allows users to easily record their expenses by inputting relevant
+                                        information such as the amount spent, date,
+                                        category, and any additional notes.</p>
+                                </div>
+                                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                                    <i class="bx bx-cube-alt"></i>
+                                    <h4>Categorization</h4>
+                                    <p>Users can categorize their expenses into different categories (e.g., food,
+                                        transportation, entertainment) to get a clear
+                                        overview of where their money is going.</p>
+                                </div>
+                                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                                    <i class="bx bx-images"></i>
+                                    <h4>Budgeting</h4>
+                                    <p>Users can set spending limits or budgets for specific categories or overall
+                                        expenses. The app can track expenses against
+                                        these budgets and provide notifications or alerts when approaching or exceeding
+                                        the limits.</p>
+                                </div>
+                                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
+                                    <i class="bx bx-shield"></i>
+                                    <h4>Financial Insights</h4>
+                                    <p>Expense tracker apps often generate visual reports and charts to give users a
+                                        clear overview of their spending habits.</p>
+                                </div>
+                                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
+                                    <i class="bx bx-atom"></i>
+                                    <h4>Multiple Accounts</h4>
+                                    <p>Users can link and manage multiple bank accounts, credit cards, or payment
+                                        platforms within the app. This feature allows
+                                        for a consolidated view of all financial transactions and facilitates easier
+                                        expense tracking.</p>
+                                </div>
+                                <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
+                                    <i class="bx bx-id-card"></i>
+                                    <h4>Customization and Personalization</h4>
+                                    <p>Users can often customize the app's interface, categories, and labels to align
+                                        with their specific financial needs and
+                                        preferences.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="image col-xl-5 d-flex align-items-stretch justify-content-center order-1 order-lg-2"
+                        data-aos="fade-left" data-aos-delay="100">
+                        <img src="assets/img/features.svg" class="img-fluid" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </section><!-- End App Features Section -->
+
+        <!-- ======= Details Section ======= -->
+        <section id="details" class="details">
+            <div class="container">
+
+                <div class="row content">
+                    <div class="col-md-4" data-aos="fade-right">
+                        <img src="assets/img/details-1.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-8 pt-4" data-aos="fade-up">
+                        <h3>Services provide by Our App</h3>
+                        <p class="fst-italic">
+                            In the context of an expense tracker app, refer to the specific features, functionalities, and capabilities
+                            that the app provides to Our users. These services are designed to assist users in effectively managing their expenses
+                            and improving their financial management. Some common services offered by out  expense tracker apps include:
+                        </p>
+                        <ul>
+                            <li><i class="bi bi-check"></i> Helps users set and manage budgets for different expense categories or overall spending 
+                            <li><i class="bi bi-check"></i> Allows users to easily record and track their expenses, including details such as date, category, amount.
+                        
+                            </li>
+                            <li><i class="bi bi-check"></i> Provides users with up-to-date insights into their spending patterns</li>
+                            <li><i class="bi bi-check"></i> Generates detailed expense reports, summaries, or visualizations</li>
+                        </ul>
+                        <p>
+                            These are just some of the services and features commonly offered by our  expense tracker app.
+                        </p>
+                    </div>
+                </div>
+
+        <!-- ======= About Us Section ======= -->
+        <section id="gallery" class="gallery">
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h2>About Us</h2>
+                    <p>Begin with a concise and engaging introduction that highlights the core purpose of the expense tracker app. Explain how the app helps users manage their finances, track expenses, and gain control over their spending habits.</p>
+                </div>
+
+            </div>
+
+            <div class="container-fluid" data-aos="fade-up">
+                <div class="gallery-slider swiper">
+                    <div class="swiper-wrapper">
+                    <div class="swiper-slide"><a href="assets/img/gallery/gallery-3.png" class="gallery-lightbox"
+                                data-gall="gallery-carousel"><img src="assets/img/gallery/gallery-2.png"
+                                    class="img-fluid" alt=""></a></div>
+                        <div class="swiper-slide"><a href="assets/img/gallery/gallery-3.png" class="gallery-lightbox"
+                                data-gall="gallery-carousel"><img src="assets/img/gallery/gallery-3.png"
+                                    class="img-fluid" alt=""></a></div>
+                        <div class="swiper-slide"><a href="assets/img/gallery/gallery-4.png" class="gallery-lightbox"
+                                data-gall="gallery-carousel"><img src="assets/img/gallery/gallery-4.png"
+                                    class="img-fluid" alt=""></a></div>
+                        </div>
+                    <div class="swiper-pagination"></div> -->
+                 </div>
+
+            </div> 
+        </section><!-- End Gallery Section -->
+
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+
+        <div class="footer-newsletter">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <h4>Subscribe Us </h4>
+                        <p>Subscribe Us for our latest updates</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h3>Expense Tracker App</h3>
+                        <p>
+                           
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Services</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Budget Management</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#"> Expense Tracking</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Customization Options</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">User Friendly Interface</a></li>
+                            
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 footer-links">
+                        <h4>Our Social Networks</h4>
+                        <!-- <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p> -->
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-4">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Expense Tracker app</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+               
+                
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
